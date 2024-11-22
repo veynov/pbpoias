8e09eaabdfde520ad50d92d01fbfeba016353b68
veynov <noobrut@gmail.com>
2024-11-22 22:09:56 +0300

    Добавлена папка Lat-master на ветку branch2

---
+++ Lat-master/Lat/libraries/openssl-master/apps/lib/libapps-lib-apps_opt_printf.d
@@ -0,0 +1,93 @@
+apps_opt_printf.c
+Note: including file: C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\apps\include\opt.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\sys/types.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\vcruntime.h
+Note: including file:     C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\sal.h
+Note: including file:      C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\concurrencysal.h
+Note: including file:     C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\vadefs.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/e_os2.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/macros.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/opensslconf.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/configuration.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/opensslv.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\stdint.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\limits.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/safestack.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/stack.h
+Note: including file:  C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\stdarg.h
+Note: including file: C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/ui.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/crypto.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\stdlib.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_malloc.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_search.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\stddef.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_wstdlib.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\time.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_wtime.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\stdio.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_wstdio.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_stdio_config.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/cryptoerr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/symhacks.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/cryptoerr_legacy.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/core.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/pem.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/bio.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/bioerr.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/evp.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/core_dispatch.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/evperr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/params.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/bn.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/bnerr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/objects.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/obj_mac.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/asn1.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/asn1err.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/objectserr.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/x509.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/buffer.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/buffererr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/ec.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\string.h
+Note: including file:      C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_memory.h
+Note: including file:       C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_memcpy_s.h
+Note: including file:        C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\errno.h
+Note: including file:        C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\include\vcruntime_string.h
+Note: including file:      C:\Program Files (x86)\Windows Kits\10\include\10.0.22000.0\ucrt\corecrt_wstring.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/ecerr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/rsa.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/rsaerr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/dsa.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/dh.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/dherr.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/dsaerr.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/sha.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/x509err.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/x509_vfy.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/lhash.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/pkcs7.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/pkcs7err.h
+Note: including file:    C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/http.h
+Note: including file:     C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/conf.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/conferr.h
+Note: including file:      C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/conftypes.h
+Note: including file:   C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/pemerr.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/types.h
+Note: including file:  C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\include\openssl/uierr.h
+Note: including file: C:\Users\cylct\OneDrive\Рабочий стол\openssl-master\apps\include\apps_ui.h
